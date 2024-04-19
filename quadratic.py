cf278a4e47155f7d5685608fb6a28d030876ff68
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-04-19 16:34:22 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,5 +1,5 @@
 # Replace the "ANSWER HERE" for your answer
-
+import math
 def roots(a, b, c):
     discriminant = b**2 - 4*a*c
 
@@ -12,7 +12,6 @@ def roots(a, b, c):
         return f'({root_11}, {root_12})'
     else:
         return '( )'
-
 
 def value_y(a, b, c, x):
     y = a*x**2 + b*x + c
@@ -31,10 +30,13 @@ def to_string(a, b, c):
     else:
         return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
-
-def derivation(a, b):
+def derivation(a, b, c):
     if a == 0:
         return f"f'(x) = {b}"
+    elif b == 0:
+        return f"f'(x) = {2*a} * X"
+    else:
+        return f"f'(x) = {a * 2} * X + {b}"
     elif b == 0:
         return f"f'(x) = {2*a} * X"
     else:
